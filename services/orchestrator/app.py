3440c7e509d8ce6176cf7c83e1e2af5cc4a6aada
k111liza <crasovaliza@yandex.ru>
2026-04-10 14:46:46 +0300

    first version of orchestrator

--- services/orchestrator/app.py
+++ services/orchestrator/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import asyncpg
 
 app = FastAPI()
 
@@ -8,6 +9,45 @@ def health():
     return {"status": "ok"}
 
 
+global db_pool
+
+
 @app.post("/train")
-def train():
-    return {"job": "created"}
+async def train(dataset_name: str, dataset_id: int):
+    async with db_pool.acqure() as conn:
+        job_id = await conn.fetchval(
+            """
+            INSERT INTO jobs (dataset_name, dataset_id, status)
+            VALUES ($1, $2, 'pending')
+            RETURNING job_id
+            """,
+            dataset_name, dataset_id
+        )
+
+        return f"job_id: {job_id}"
+
+
+@app.get("/jobs/job_id")
+async def gut_status(job_id: int):
+    async with db_pool.acqure() as conn:
+        status = await conn.fetchval(
+            """
+            SELECT (status)
+            from jobs
+            WHERE job_id = $1
+            """,
+            job_id
+        )
+
+        return f"status: {status}"
+
+
+@app.post("/datasets")
+async def register_dataset():
+    async with db_pool.acqure() as conn:
+        reg = await conn.fetchval(
+            """
+            INSET INTO trained_models ()
+            """
+        )
+        return {"status": "ok"}
